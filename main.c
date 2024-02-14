2b3ef034191e538adc79e55187f531b14ae5bef5
Edgar Marques <edgar.eu1@hotmail.com>
2024-02-14 01:46:25 +0000

    Init #2

--- main.c
+++ main.c
@@ -15,25 +15,30 @@
 #define LOOP for (int i = 0; i < MAX_CLIENTS; i++)
 #define UNUSED -1
 
-typedef struct s_client {
-	int	id;
-	int	fd;
-	char*	buffer;
-} t_client;
+typedef struct	s_client {
+	int			id;
+	int			fd;
+	char		*buffer;
+}				t_client;
 
-void	error(char* msg) {
+int	last_id = -1;
+
+void	error(char *msg)
+{
 	write(STDERR_FILENO, msg, strlen(msg));
 	exit(1);
 }
 
-void	broadcast(t_client* clients, char* msg, int id) {
+void	broadcast(t_client *clients, char *msg, int id)
+{
 	LOOP {
-		if (clients[i].fd > UNUSED && clients[i].id != id)
+		if ((clients[i].fd > UNUSED) && (clients[i].id != id))
 			send(clients[i].fd, msg, strlen(msg), 0);
 	}
 }
 
-void	initClient(t_client* client) {
+void	initClient(t_client *client)
+{
 	client->id = UNUSED;
 	client->fd = UNUSED;
 	client->buffer = NULL;
@@ -42,7 +47,7 @@ void	initClient(t_client* client) {
 int extract_message(char **buf, char **msg)
 {
 	char	*newbuf = NULL;
-	int	i;
+	int		i;
 
 	*msg = 0;
 	if (*buf == 0)
@@ -86,23 +91,23 @@ char *str_join(char *buf, char *add)
 	return (newbuf);
 }
 
-int	last_id = -1;
-
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
 	if (argc != 2)
 		error(ERR_ARGS);
 
-	int	server_fd;
-	char	buffer[BUFFER_SIZE + 1];
+	int			server_fd;
+	char		buffer[BUFFER_SIZE + 1];
 	t_client	clients[MAX_CLIENTS];
+
 	LOOP { initClient(&clients[i]); }
 
 	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
 		error(ERR_SYS);
 	
-	fd_set	read_fds;
-
+	fd_set				read_fds;
 	struct sockaddr_in	server_addr;
+
 	server_addr.sin_family = AF_INET; 
 	server_addr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
 	server_addr.sin_port = htons(atoi(argv[1])); 
@@ -112,7 +117,8 @@ int main(int argc, char** argv) {
 	if (listen(server_fd, MAX_CLIENTS) != 0)
 		error(ERR_SYS);
 
-	while (42) {
+	while (42)
+	{
 		FD_ZERO(&read_fds);
 		FD_SET(server_fd, &read_fds);
 		int	max_fd = server_fd;
@@ -126,13 +132,15 @@ int main(int argc, char** argv) {
 		if (select(max_fd + 1, &read_fds, NULL, NULL, NULL) < 0)
 			error(ERR_SYS);
 
-		if (FD_ISSET(server_fd, &read_fds)) {
+		if (FD_ISSET(server_fd, &read_fds))
+		{
 			int	new_fd = accept(server_fd, NULL, NULL);
 			if (new_fd < 0)
 				error(ERR_SYS);
 
 			LOOP {
-				if (clients[i].fd < 0) {
+				if (clients[i].fd < 0)
+				{
 					clients[i].id = last_id + 1; last_id++;
 					clients[i].fd = new_fd;
 					memset(buffer, 0, BUFFER_SIZE);
@@ -144,12 +152,14 @@ int main(int argc, char** argv) {
 		}
 
 		LOOP {
-			if (FD_ISSET(clients[i].fd, &read_fds)) {
+			if (FD_ISSET(clients[i].fd, &read_fds))
+			{
 				memset(buffer, 0, BUFFER_SIZE);
 				int	n = recv(clients[i].fd, buffer, BUFFER_SIZE, 0);
 				if (n < 0)
 					error(ERR_SYS);
-				else if (n == 0) {
+				else if (n == 0)
+				{
 					sprintf(buffer, LOG_DISCONNECT, clients[i].id);
 					broadcast(clients, buffer, clients[i].id);
 					close(clients[i].fd);
@@ -167,8 +177,9 @@ int main(int argc, char** argv) {
 					}
 				}	
 			}
-			while (clients[i].buffer && strstr(clients[i].buffer, "\n")) {
-				char*	msg = NULL;
+			while (clients[i].buffer && strstr(clients[i].buffer, "\n"))
+			{
+				char	*msg = NULL;
 				extract_message(&clients[i].buffer, &msg);
 				char	output[strlen(msg) + 16];
 				sprintf(output, "client %d: %s", clients[i].id, msg);
@@ -177,6 +188,7 @@ int main(int argc, char** argv) {
 			}
 		}
 	}
+
 	close(server_fd);
 	return 0;
 }
